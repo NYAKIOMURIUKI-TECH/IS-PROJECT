50a1ffc5512a12868395d82a58e91e22f1b37719
ogororuth <ruth.ogoro@strathmore.edu>
2024-07-09 01:37:00 +0300

    service entity

---
+++ backend/app/models/service.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+from app.models.base_model import Base, BaseModel
+
+class Service(BaseModel, Base):
+    __tablename__ = 'services'
+    name = Column(String(25), nullable=False)
+    description = Column(String(128), nullable=False)
+    price = Column(Integer)
+
+    workers = relationship('Worker', back_populates='service')
+    bookings = relationship('Booking', back_populates='service')
